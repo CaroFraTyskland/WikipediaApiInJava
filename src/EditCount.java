86ad7e17f974c1159a7096aec4babc81c687b152
CaroFraTyskland <bruckmeier@fim.uni-passau.de>
2022-02-23 19:33:26 +0100

    Initial commit: class EditCount

---
+++ src/EditCount.java
@@ -0,0 +1,45 @@
+/**
+ * Contains methods to retrieve the edit count of users.
+ */
+public class EditCount {
+
+    /**
+     * Returns the edit count of a given user.
+     *
+     * @param userName     the user's name (should only contain encoded special characters)
+     * @param wikiLanguage the language of the Wikipedia (e.g. "en", "de")
+     * @return the number of edits
+     */
+    public static int getEditCount(String userName, String wikiLanguage) {
+        String apiResponse = getEditCountFromApi(userName, wikiLanguage);
+
+        if (apiResponse == null) {
+            return 0;
+        }
+
+        int indexStart = apiResponse.indexOf("editcount\":") + 11;
+        int indexEnd = apiResponse.indexOf("}]");
+
+        if (indexEnd < indexStart) {
+            return 0;
+        }
+
+        return Integer.parseInt(apiResponse.substring(indexStart, indexEnd));
+    }
+
+    /**
+     * Retrieves the JSON data containing the edit count of the given user in the given Wikipedia project.
+     *
+     * @param userName     the user's name
+     * @param wikiLanguage the language of the Wikipedia project (e.g. "en", "de")
+     * @return the API response in JSON format
+     */
+    private static String getEditCountFromApi(String userName, String wikiLanguage) {
+        userName = userName.replaceAll(" ", "+");
+
+        String url = "https://" + wikiLanguage + ".wikipedia.org/w/api.php?action=query&list=users&ususers=" + userName
+                + "&usprop=editcount&format=json";
+
+        return HttpRequestHelper.makeGetRequest(url);
+    }
+}
---
+++ src/HttpRequestHelper.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class HttpRequestHelper {
+
+    /**
+     * Makes the get request to the API and returns the response.
+     *
+     * @param urlString the URL for the request
+     * @return the response
+     */
+    public static String makeGetRequest(String urlString) {
+        String response = null;
+
+        try {
+            URL url = new URL(urlString);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            response = reader.readLine();
+            conn.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+}
